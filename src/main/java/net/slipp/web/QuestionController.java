105d20a28d5986643d157abef082056b027b570a
KOOSANGYOON <nevergiveup936@naver.com>
2018-03-06 17:46:07 +0900

    add exception code to reduce code length.(refactoring)

--- src/main/java/net/slipp/web/QuestionController.java
+++ src/main/java/net/slipp/web/QuestionController.java
@@ -21,80 +21,84 @@ import net.slipp.domain.User;
 public class QuestionController {
 	@Autowired
 	private QuestionRepository questionRepository;
-	
+
 	@GetMapping("/qnaform")
 	public String qnaform() {
 		return "/qna/qnaform";
 	}
-	
+
 	@PostMapping("")
 	public String qnaSubmit(String title, String contents, HttpSession session) {
 		if (!HttpSessionUtils.isLoginUser(session)) {
 			return "redirect:/user/loginForm";		//redirect 하지 않으면, PostMappnig -> GetMapping 해야하는데, 이 부분을 구현해놓지 않았으므로, 405 에러가 뜬다.
 		}
-		
+
 		User sessionUser = HttpSessionUtils.getUserFromSession(session);
 		Question newQuestion = new Question(sessionUser, title, contents);
 		questionRepository.save(newQuestion);
-		
+
 		System.out.println("question : " + newQuestion);
-		
+
 		return "redirect:/";
 	}
-	
+
 	@GetMapping("/{id}")
 	public String readQuestion(@PathVariable long id, Model model) {
 		model.addAttribute("question", questionRepository.findOne(id));
 		return "/qna/show";
 	}
-	
+
 	@GetMapping("/{id}/form")
 	public String updateForm(@PathVariable Long id, Model model, HttpSession session) {
-		if (!HttpSessionUtils.isLoginUser(session)) {
-			return "redirect:/user/loginForm";		//login 되어있는지 확인. qnaSubmit() 에서 복, 붙
+		try {
+			Question question = questionRepository.findOne(id);
+			hasPermission(session, question);
+			model.addAttribute("question", question);
+			return "/qna/updateForm";
+
+		}catch(IllegalStateException e) {
+			model.addAttribute("errorMessage", e.getMessage());
+			return "/user/login";
 		}
-		
-		User loginUser = HttpSessionUtils.getUserFromSession(session);
-		Question question = questionRepository.findOne(id);
-		if (!question.isSameWriter(loginUser)) {
-			return "redirect:/user/loginForm";
-		}
-		
-		model.addAttribute("question", question);
-		System.out.println(question);
-		return "/qna/updateForm";
 	}
-	
+
+	private boolean hasPermission(HttpSession session, Question question) {
+		if (!HttpSessionUtils.isLoginUser(session)) {
+			throw new IllegalStateException("로그인이 필요합니다.");
+		}
+
+		User loginUser = HttpSessionUtils.getUserFromSession(session);
+		if (!question.isSameWriter(loginUser)) {
+			throw new IllegalStateException("자신이 쓴 글만 수정, 삭제가 가능합니다.");
+		}
+
+		return true;
+	}
+
 	@PutMapping("/{id}")
-	public String update(@PathVariable Long id, String title, String contents, HttpSession session) {
-		if (!HttpSessionUtils.isLoginUser(session)) {
-			return "redirect:/user/loginForm";		//login 되어있는지 확인. qnaSubmit() 에서 복, 붙
+	public String update(@PathVariable Long id, String title, String contents, Model model, HttpSession session) {
+		try {
+			Question question = questionRepository.findOne(id);
+			hasPermission(session, question);
+			question.update(title, contents);
+			questionRepository.save(question);
+			return String.format ("redirect:/questions/%d", id);
+		}catch(IllegalStateException e) {
+			model.addAttribute("errorMessage", e.getMessage());
+			return "/user/login";
 		}
-		
-		User loginUser = HttpSessionUtils.getUserFromSession(session);
-		Question question = questionRepository.findOne(id);
-		if (!question.isSameWriter(loginUser)) {
-			return "redirect:/user/loginForm";
-		}
-		
-		question.update(title, contents);
-		questionRepository.save(question);
-		return String.format ("redirect:/questions/%d", id);
 	}
-	
+
 	@DeleteMapping("/{id}")
-	public String delete(@PathVariable Long id, HttpSession session) {
-		if (!HttpSessionUtils.isLoginUser(session)) {
-			return "redirect:/user/loginForm";		//login 되어있는지 확인. qnaSubmit() 에서 복, 붙
+	public String delete(@PathVariable Long id, Model model, HttpSession session) {
+		try {
+			Question question = questionRepository.findOne(id);
+			hasPermission(session, question);
+			questionRepository.delete(id);
+			return "redirect:/";
+		}catch(IllegalStateException e) {
+			model.addAttribute("errorMessage", e.getMessage());
+			return "/user/login";
 		}
-		
-		User loginUser = HttpSessionUtils.getUserFromSession(session);
-		Question question = questionRepository.findOne(id);
-		if (!question.isSameWriter(loginUser)) {
-			return "redirect:/user/loginForm";
-		}
-		
-		questionRepository.delete(id);
-		return "redirect:/";
 	}
 }
